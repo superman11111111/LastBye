0612dd1ad484ca60b77e9490182e2d7b275a549c
root <root@vmi359523.contaboserver.net>
2020-10-11 21:00:46 +0200

    cccc

--- lastbye.py
+++ lastbye.py
@@ -1,6 +1,6 @@
 from subprocess import check_output
 
-DUMP_FILE = 'lastb-dump.txt'
+DUMP_FILE = 'lastb.dump'
 dmp = open(DUMP_FILE, 'w+')
 old_ips = dmp.read().split('\n')
 ips = [line.split()[2] for line in check_output(['lastb']).decode().split("\n")[:-3] if len(line.split()) == 10]
